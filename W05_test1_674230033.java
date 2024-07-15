dec4f4bd349af9743f293ec1a95a255704c3fd51
674230033 <yomjai1234@gmail.com>
2024-07-15 12:17:08 +0700

    Add files via upload

---
+++ W05_test1_674230033.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class W05_test1_674230033 {
+    public static void main (String[] args){
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("ค่าความยาวของเส้นทเเยงมุม:");
+
+        int พื้นที่ = 800;
+        int ทเเยงมุมที่1 = 200; 
+        int ทเเยงมุมที่2 = 200;
+        int area = 200 * 200;
+        System.out.println("ฐาน * สูง = พื้นที่ของขนมเปียกปูน");
+        
+
+    }
+}
